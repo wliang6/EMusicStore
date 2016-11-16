050de41aa354d62446dacab69fdfa9141908cb9d
Winnie Liang <wliang6@buffalo.edu>
2016-11-16 13:29:01 -0500

    First dive into project. Add Bootstrap frontend

---
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.emusicstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+}
---
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package com.emusicstore.dao;
+
+/**
+ * EMusicStore
+ * @author Winnie Nov 16, 2016
+ * ProductDao.java
+ * Dao - Data Accessing Object
+ */
+public class ProductDao {
+	
+}
